12ff79adbb92b11566fd89f2f92b5b58c1482e27
Michael Orlando <mikeyo4800@gmail.com>
2015-01-01 19:00:23 -0500

    Implemented devise for users

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,5 +25,9 @@ Pinteresting::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
+
   config.assets.debug = true
+
+# Line 32 used for heroku
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
