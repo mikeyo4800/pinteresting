8eb672bf5038cfab7edb60ace49d8a2e3653f96e
Michael Orlando <mikeyo4800@gmail.com>
2015-02-09 19:36:47 -0500

    Added amazon shit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,17 @@ Pinteresting::Application.configure do
 
 
 config.action_mailer.default_url_options = { :host => 'thebossofpinteresting.herokuapp' }
+
+#sets paperclip to upload images in s3
+config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
+
+
+
 end
